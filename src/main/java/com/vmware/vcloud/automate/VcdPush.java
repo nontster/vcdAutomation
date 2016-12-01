2001f74ecaf65653c74d18a76de5daf8ad705914
Nont Banditwong <nontster@gmail.com>
2016-12-01 15:03:23 +0700

    Reorganize help argument

--- src/main/java/com/vmware/vcloud/automate/VcdPush.java
+++ src/main/java/com/vmware/vcloud/automate/VcdPush.java
@@ -63,16 +63,9 @@ public class VcdPush {
 		CommandLineParser parser = new DefaultParser();
 		Console cnsl = null;
 
-		if (args.length < 1) {
-			System.err.println("Type Bee -help for usage.");
-			return;
-		} else if (args[0].equals("-v") || args[0].equals("--version")) {
-			System.out.println("Bee version 1.1.1");
-			return;
-		}
 	      
-		Option optHelp = new Option("help", "print this message");
-		Option optDebug = new Option("debug", "print debugging information");
+		Option optHelp = Option.builder("h").longOpt("help").desc("print usage").hasArg(false).required(false)
+				.argName("help").build();
 
 		Option optBlueprint = Option.builder("t").longOpt("template").desc("template file").hasArg(true)
 				.required(true).argName("file").build();
@@ -86,16 +79,30 @@ public class VcdPush {
 		Option optPassword = Option.builder("p").longOpt("password").desc("password").hasArg(true).required(false)
 				.argName("password").build();
 		
-		Option optOutput = Option.builder("o").longOpt("output").desc("Output").hasArg(true).required(false)
+		Option optOutput = Option.builder("o").longOpt("output").desc("output").hasArg(true).required(false)
 				.argName("output").build();
-	
+
+		Option optVersion = Option.builder("v").longOpt("version").desc("version").hasArg(false).required(false)
+				.argName("version").build();
+		
 		options.addOption(optBlueprint);
 		options.addOption(optVcdurl);
 		options.addOption(optUsername);
 		options.addOption(optPassword);
 		options.addOption(optHelp);
-		options.addOption(optDebug);
 		options.addOption(optOutput);
+		options.addOption(optVersion);	
+		
+		if (args.length < 1) {
+			System.err.println("Type Bee -help for usage.");
+			return;
+		} else if (args[0].equals("-v") || args[0].equals("--version")) {
+			System.out.println("Bee version 1.1.1");
+			return;
+		} else if(args[0].equals("-h") || args[0].equals("--help")){
+			formatter.printHelp("bee", options); 
+			return;
+		}		
 		
 		try {
 			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
@@ -106,142 +113,133 @@ public class VcdPush {
 						
 			CommandLine cmd = parser.parse(options, args);
 
-			if (cmd.hasOption("version")){
-				System.out.println("Bee version 1.1.1");
+			if (cmd.hasOption("vcdurl")) {
+				vcdurl = cmd.getOptionValue("vcdurl");
 			}
-			else if (cmd.hasOption("help")){
-				/* automatically generate the help statement */
-				formatter.printHelp("bee", options); 
-			} else {
 
-				if (cmd.hasOption("vcdurl")) {
-					vcdurl = cmd.getOptionValue("vcdurl");
-				}
+			if (cmd.hasOption("user")) {
+				username = cmd.getOptionValue("user");
 
-				if (cmd.hasOption("user")) {
-					username = cmd.getOptionValue("user");
-					
-					int index = username.indexOf('@');
-					
-					if(index < 0)
-						username += "@System"; 
-				}
+				int index = username.indexOf('@');
 
-				if (cmd.hasOption("password")) {
-					password = cmd.getOptionValue("password");
-				}
+				if (index < 0)
+					username += "@System";
+			}
 
-				if (cmd.hasOption("template")) {
-					template = cmd.getOptionValue("template");
-				}
-				
-				if (cmd.hasOption("output")) {
-					output = cmd.getOptionValue("output");
-				}
+			if (cmd.hasOption("password")) {
+				password = cmd.getOptionValue("password");
+			}
 
-				if(vcdurl == null){
-					vcdurl = prop.getProperty("url");
-				}
-				
-				if(vcdurl.charAt(vcdurl.length()-1)!='/'){
-					vcdurl += '/';
-				}
-				
-				if (password == null) {					
-					// creates a console object
-			         cnsl = System.console();
+			if (cmd.hasOption("template")) {
+				template = cmd.getOptionValue("template");
+			}
 
-			         // if console is not null
-			         if (cnsl != null) {
-			        	// read password into the char array
-			             char[] pwd = cnsl.readPassword("Enter your password: ");
-			             password = new String(pwd);
-			         }				
-				}
-				
-				if(output == null){
-					Path currentRelativePath = Paths.get("");
-					output = currentRelativePath.toAbsolutePath().toString() + File.separator + "report.xlsx";			
-				}
+			if (cmd.hasOption("output")) {
+				output = cmd.getOptionValue("output");
+			}
 
-				ConfigParser cParser = ConfigParser.getParser(template);
-				cParser.validate();
-				vCloudOrg = cParser.getOrg();
-				
-				// Check template version
-				if(!vCloudOrg.getTemplate_version().equalsIgnoreCase("2016-10-25")){
-					System.err.println("Invalid template version!");
-					System.exit(1);
+			if (vcdurl == null) {
+				vcdurl = prop.getProperty("url");
+			}
+
+			if (vcdurl.charAt(vcdurl.length() - 1) != '/') {
+				vcdurl += '/';
+			}
+
+			if (password == null) {
+				// creates a console object
+				cnsl = System.console();
+
+				// if console is not null
+				if (cnsl != null) {
+					// read password into the char array
+					char[] pwd = cnsl.readPassword("Enter your password: ");
+					password = new String(pwd);
 				}
+			}
+
+			if (output == null) {
+				Path currentRelativePath = Paths.get("");
+				output = currentRelativePath.toAbsolutePath().toString() + File.separator + "report.xlsx";
+			}
+
+			ConfigParser cParser = ConfigParser.getParser(template);
+			cParser.validate();
+			vCloudOrg = cParser.getOrg();
+
+			// Check template version
+			if (!vCloudOrg.getTemplate_version().equalsIgnoreCase("2016-10-25")) {
+				System.err.println("Invalid template version!");
+				System.exit(1);
+			}
+
+			VcloudClient.setLogLevel(Level.OFF);
+			System.out.println("Vcloud Login");
+			client = new VcloudClient(vcdurl, Version.V5_5);
+
+			client.login(username, password);
+			System.out.println("	Login Success\n");
+
+			System.out.println("Get Vcloud Admin");
+			admin = client.getVcloudAdmin();
+			System.out.println("	" + admin.getResource().getHref() + "\n");
+
+			System.out.print("Add New Organization : ");
+			AdminOrganization adminOrg = admin.createAdminOrg(OrgUtils.createNewAdminOrgType(vCloudOrg));
+			Task task = OrgUtils.returnTask(client, adminOrg);
+			if (task != null)
+				task.waitForTask(0);
+			System.out.print(adminOrg.getResource().getName() + " : ");
+			System.out.println(adminOrg.getResource().getHref() + "\n");
+
+			// Set vCloud director URL for organization
+			vCloudOrg.setUrl(vcdurl + "cloud/org/" + vCloudOrg.getName() + "/");
+
+			// Create vDC You may end using one of the following.
+			adminVdc = VdcUtils.addPayAsYouGoVdc(vCloudOrg, admin, client, adminOrg, prop);
+
+			// Create user on the organization
+			UserUtils.addUserToOrg(vCloudOrg, admin, adminOrg);
+
+			// Create org vdc networks on the organizaiton
+			edgeGateway = NetworkUtils.addNatRoutedOrgVdcNetwork(client, vCloudOrg, adminVdc, adminOrg, prop);
+
+			// find the vdc ref
+			Vdc vdc = Vdc.getVdcByReference(client, adminVdc.getVdcReference());
+
+			// find the vapp template ref
+			String catalogName = null;
+
+			if (vCloudOrg.getCloudResources() != null && vCloudOrg.getCloudResources().getCatalog() != null
+					&& vCloudOrg.getCloudResources().getCatalog().getName() != null)
+				catalogName = vCloudOrg.getCloudResources().getCatalog().getName();
+			else
+				catalogName = prop.getProperty("catalog");
+
+			// Composed vApp.
+			if (vCloudOrg.getvApp() != null && vCloudOrg.getvApp().getName() != null)
+				System.out.println("vApp: " + vCloudOrg.getvApp().getName());
+			else
+				System.out.println("vApp: vApp_system_1");
+
+			Vapp vapp = vdc.composeVapp(VappUtils.createComposeParams(client, vCloudOrg, catalogName, vdc));
+			System.out.print("	Composing vApp : " + vapp.getResource().getName());
+			List<Task> tasks = vapp.getTasks();
+			if (tasks.size() > 0)
+				tasks.get(0).waitForTask(0);
+
+			// refresh the vapp
+			vapp = Vapp.getVappByReference(client, vapp.getReference());
+
+			System.out.println(" - " + vapp.getResource().getHref());
+
+			// reconfigure Vms
+			VappUtils.reconfigureVms(vapp, vCloudOrg);
+
+			// generate report
+			ReportUtils.generateReport(client, vapp, edgeGateway, vCloudOrg, adminVdc.getResource().getName(), output);
+			System.out.println("---------- Completed! ----------");
 			
-				VcloudClient.setLogLevel(Level.OFF);
-				System.out.println("Vcloud Login");
-				client = new VcloudClient(vcdurl, Version.V5_5);
-
-				client.login(username, password);
-				System.out.println("	Login Success\n");
-
-				System.out.println("Get Vcloud Admin");
-				admin = client.getVcloudAdmin();
-				System.out.println("	" + admin.getResource().getHref() + "\n");
-				
-				System.out.print("Add New Organization : ");
-				AdminOrganization adminOrg = admin.createAdminOrg(OrgUtils.createNewAdminOrgType(vCloudOrg));
-				Task task = OrgUtils.returnTask(client, adminOrg);
-				if (task != null)
-					task.waitForTask(0);
-				System.out.print(adminOrg.getResource().getName() + " : ");
-				System.out.println(adminOrg.getResource().getHref() + "\n");
-
-				// Set vCloud director URL for organization
-				vCloudOrg.setUrl(vcdurl + "cloud/org/" + vCloudOrg.getName() +"/");
-				
-				// Create vDC You may end using one of the following.
-				adminVdc = VdcUtils.addPayAsYouGoVdc(vCloudOrg, admin, client, adminOrg, prop);
-
-				// Create user on the organization
-				UserUtils.addUserToOrg(vCloudOrg, admin, adminOrg);
-
-				// Create org vdc networks on the organizaiton
-				edgeGateway = NetworkUtils.addNatRoutedOrgVdcNetwork(client, vCloudOrg, adminVdc, adminOrg, prop);
-
-				// find the vdc ref
-				Vdc vdc = Vdc.getVdcByReference(client, adminVdc.getVdcReference());
-				
-				// find the vapp template ref
-				String catalogName = null;
-				
-				if (vCloudOrg.getCloudResources() != null 
-						&& vCloudOrg.getCloudResources().getCatalog() != null
-						&& vCloudOrg.getCloudResources().getCatalog().getName() != null)
-					catalogName = vCloudOrg.getCloudResources().getCatalog().getName();
-				else
-					catalogName = prop.getProperty("catalog");
-				
-				// Composed vApp. 
-				if (vCloudOrg.getvApp() != null && vCloudOrg.getvApp().getName() != null)
-					System.out.println("vApp: " + vCloudOrg.getvApp().getName());
-				else
-					System.out.println("vApp: vApp_system_1");
-				
-				Vapp vapp = vdc.composeVapp(VappUtils.createComposeParams(client, vCloudOrg, catalogName, vdc));
-				System.out.print("	Composing vApp : " + vapp.getResource().getName());
-				List<Task> tasks = vapp.getTasks();
-				if (tasks.size() > 0)
-					tasks.get(0).waitForTask(0);
-				
-				// refresh the vapp
-				vapp = Vapp.getVappByReference(client, vapp.getReference());
-				
-				System.out.println(" - " + vapp.getResource().getHref());
-				
-				// reconfigure Vms
-				VappUtils.reconfigureVms(vapp, vCloudOrg);
-										
-				// generate report
-				ReportUtils.generateReport(client, vapp, edgeGateway, vCloudOrg, adminVdc.getResource().getName(), output);
-				System.out.println("---------- Completed! ----------");
-			}
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			System.err.println("Parsing failed.  Reason: " + e.getMessage());
